6215b25d048c92f06b16576c0c5aabc031ccc0b5
codebeatme <codebeatme@outlook.com>
2023-12-23 11:37:20 +0800

    归档

---
+++ src/zh-hant/part_4/escape.js
@@ -0,0 +1,9 @@
+/*
+本節文章
+https://learnscript.net/zh-hant/programming/part-4/escape-characters-and-sequences/ 什麼是逸出字元，逸出序列
+*/
+
+// 使用逸出序列產生一個購物清單
+const table = "物品\t\t數量\n蘋果\t\t10（個）\n牛奶\t\t1（箱）"
+
+console.log("以下是今天的購物清單：\n" + table)
--- src/zh/Program.cs
+++ src/zh/Program.cs
@@ -1,10 +1,3 @@
-﻿// 导入命名空间 World.Animal
-using World.Animals;
-
-// 创建命名空间 World.Animal 中的 Anemone 类的实例
-Anemone anemone1 = new();
-anemone1.Run();
-
-// 直接使用命名空间 World.Plant 创建实例
-World.Plants.Anemone anemone2 = new();
-anemone2.Grow();
+﻿Console.WriteLine(0.1f == 0.1);
+Console.WriteLine(10000000000000000000f == 10000000000000000000);
+Console.WriteLine(0.123456789523456789f);
